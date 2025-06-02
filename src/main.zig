8e5d16978131eb2f8931f217ed8b4975c5765d08
adambigg-s <biggsa601@gmail.com>
2025-06-02 16:24:06 -0400

    vector bug fix

--- src/main.zig
+++ src/main.zig
@@ -3,13 +3,13 @@ const std = @import("std");
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
-    var world = try lib.World.build(240, 55, allocator);
+    var world = try lib.World.build(600, 200, allocator);
     world.randomize();
 
     while (true) {
         try world.display();
         try world.update();
 
-        std.Thread.sleep(50 * std.time.ns_per_ms);
+        std.Thread.sleep(1 * std.time.ns_per_ms);
     }
 }
